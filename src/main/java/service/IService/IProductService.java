e35135ceafa6965db87d5c6c2b64c7ba45f33402
Leduc2910 <minhduc293103@gmail.com>
2023-11-29 21:01:17 +0700

    Duc create IService - 29/11

--- src/main/java/service/IService/IProductService.java
+++ src/main/java/service/IService/IProductService.java
@@ -1,4 +1,12 @@
 package service.IService;
 
-public class IProductService {
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
+
+import java.util.List;
+
+public interface IProductService<E> {
+    void add(E e);
+    void edit (int id, E e);
+    void delete(int id);
+    List<E> findAll();
 }
--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -1,4 +1,28 @@
 package service;
 
-public class ProductService {
+import model.Product;
+import service.IService.IProductService;
+
+import java.util.List;
+
+public class ProductService implements IProductService<Product> {
+    @Override
+    public void add(Product product) {
+
+    }
+
+    @Override
+    public void edit(int id, Product product) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return null;
+    }
 }
