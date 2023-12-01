633c1e77720e539a65c0040756eb71908a394433
phunglam98 <duclam9812@gmail.com>
2023-12-01 20:02:56 +0700

    lâm sửa categoryService

--- src/main/java/service/CategoryService.java
+++ src/main/java/service/CategoryService.java
@@ -3,26 +3,81 @@ package service;
 import model.Category;
 import service.IService.ICategoryService;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CategoryService implements ICategoryService<Category> {
+    Connection connection = ConnectToMySQL.getConnection();
+
     @Override
     public void add(Category category) {
+        String sql = "insert into category(name) values (?)";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, category.getName());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @Override
     public void edit(int id, Category category) {
+        String sql = "update category set name = ?  where id = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, category.getName());
+            preparedStatement.setInt(2,id);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 
     @Override
     public void delete(int id) {
+        String sql = "delete from category where id = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 
     @Override
     public List<Category> findAll() {
-        return null;
+        String sql = "select * from category";
+        List<Category> categoryList = new ArrayList<>();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                Category category = new Category(id, name);
+                categoryList.add(category);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return categoryList;
     }
 }
