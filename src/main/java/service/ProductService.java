34a2a688df0f134b34f8d2abeeec46fdc7fae1c7
hieu19491 <mariopuzo19491@gmail.com>
2023-12-01 19:25:36 +0700

    Hiếu build ProductService - 1/12

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -1,13 +1,36 @@
 package service;
 
+import model.Category;
 import model.Product;
 import service.IService.IProductService;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductService implements IProductService<Product> {
+    private Connection connection = ConnectToMySQL.getConnection();
+
     @Override
     public void add(Product product) {
+    String sql = "insert into product(name, brand, unit, weight, price, description, image, idcategory) values (?, ?, ?, ?, ?, ?, ?, ?);";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, product.getName());
+            preparedStatement.setString(2, product.getBrand());
+            preparedStatement.setString(3, product.getUnit());
+            preparedStatement.setDouble(4, product.getWeight());
+            preparedStatement.setInt(5, product.getPrice());
+            preparedStatement.setString(6, product.getDescription());
+            preparedStatement.setString(7, product.getImage());
+            preparedStatement.setInt(8, product.getCategory().getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -23,6 +46,29 @@ public class ProductService implements IProductService<Product> {
 
     @Override
     public List<Product> findAll() {
-        return null;
+        List<Product> productList = new ArrayList<>();
+        String sql = "select product.*, c.name as nameCategory from product join category c join store.product p on c.id = p.idCategory;";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String brand = resultSet.getString("brand");
+                String unit = resultSet.getString("unit");
+                int weight = resultSet.getInt("weight");
+                int price = resultSet.getInt("price");
+                String description = resultSet.getString("description");
+                String image = resultSet.getString("image");
+                int idCategory = resultSet.getInt("idCategory");
+                String nameCategory = resultSet.getString("nameCategory");
+                Category category = new Category(idCategory, nameCategory);
+                Product product = new Product(id, name, brand, unit, weight, price, description, image, category);
+                productList.add(product);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return productList;
     }
 }
