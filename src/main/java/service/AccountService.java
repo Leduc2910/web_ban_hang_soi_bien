23f5fc915b742503eabe4e367ff4fc83af9e1f01
Leduc2910 <minhduc293103@gmail.com>
2023-12-04 19:39:24 +0700

    duc create account

---
+++ src/main/java/service/AccountService.java
@@ -0,0 +1,28 @@
+package service;
+
+import model.Account;
+import service.IService.IAccountService;
+
+import java.util.List;
+
+public class AccountService implements IAccountService<Account> {
+    @Override
+    public void checkAccount(Account account) {
+
+    }
+
+    @Override
+    public void edit(int id, Account account) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Account> findAll() {
+        return null;
+    }
+}
