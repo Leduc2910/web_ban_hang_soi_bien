232abc54c0633e42c25bb61d82884e1341d26a00
Leduc2910 <minhduc293103@gmail.com>
2023-12-04 19:49:19 +0700

    create account - 4/12

--- src/main/java/controller/AccountController.java
+++ src/main/java/controller/AccountController.java
@@ -12,11 +12,33 @@ import java.io.IOException;
 public class AccountController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        switch (action) {
+            case "showAllAccount":
+                showAllAccount(req, resp);
+                break;
+            case "editAccount":
+                showFormEdit(req, resp);
+                break;
+        }
+    }
 
+    private void showFormEdit(HttpServletRequest req, HttpServletResponse resp) {
+    }
+
+    private void showAllAccount(HttpServletRequest req, HttpServletResponse resp) {
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        switch (action) {
+            case "editAccount":
+                editAccount(req, resp);
+                break;
+        }
+    }
 
+    private void editAccount(HttpServletRequest req, HttpServletResponse resp) {
     }
 }
